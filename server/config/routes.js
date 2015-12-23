5a5b72096d8951ccfeed80c90d0adec6c5fa77ff
Eric Chou <ericchou2010@gmail.com>
2015-12-22 16:59:33 -0800

    server controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,4 +7,32 @@ module.exports = function(app) {
   // app.get('/questions/:id', questions.getQuestion);
   // app.post('/questions/:id', questions.createAnswer);
   // app.post('/like/:id', questions.likeAnswer);
+  app.get('/', function(req, res){
+  		res.render('index');
+  	})
+    //login.html routes
+	app.post('/checkUserExists/:any', function(req, res){
+		users.checkUserExists(req, res);
+	})
+	app.post('/addUser', function(req, res){
+		users.addUser(req, res);
+	})
+  //dashboard.html routes
+  app.post('/addEvent', function(req, res){
+		events.addEvent(req, res);
+	})
+  app.get('/getEvents', function(req, res){
+	  events.getEvents(req, res);
+	})
+  //event
+	app.get('/getEventById/:id', function(req, res){
+		events.getEventById(req, res);
+	})
+  //comments
+  app.post('/addComment', function(req, res){
+		comments.addComment(req, res);
+	})
+	app.get('/getCommentsByPostId/:id', function(req, res)	{
+		comments.getCommentsByPostId(req, res);
+	})
 };
