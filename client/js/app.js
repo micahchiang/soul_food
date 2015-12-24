e76f70087a3c90b935a7cb8e396077e5555a964d
micah.chiang <micahkchiang@gmail.com>
2015-12-23 19:05:06 -0800

    added basic parallax to main.html

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,11 @@
-var soleFood = angular.module('soleFood', ['ngRoute'])
+var soulFood = angular.module('soulFood', ['ngRoute'])
   .config(function($routeProvider){
     $routeProvider
     .when('/', {
-      templateUrl: '/partials/main.html'
+      templateUrl: '/partials/main.html', controller: 'mainController'
+    })
+    .when('/main', {
+      templateUrl: '/partials/main.html', controller: 'mainController'
     })
     .when('/login', {
       templateUrl: '/partials/login.html'
@@ -11,3 +14,5 @@ var soleFood = angular.module('soleFood', ['ngRoute'])
       templateUrl: '/partials/add.html'
     })
   })
+
+
---
+++ client/js/controllers/mainController.js
@@ -0,0 +1,9 @@
+soulFood.controller('mainController', function($scope, $location, $anchorScroll){
+	$scope.scrollTo = function(id){
+		$location.hash(id);
+		$anchorScroll();
+		$location.hash(old);
+	}
+
+
+});
