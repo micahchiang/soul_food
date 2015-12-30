5661aa3e20be0ef54ae1588f1a0c448a8eb893d8
micah.chiang <micahkchiang@gmail.com>
2015-12-30 13:22:24 -0500

    adjusted app.js, added styles to dashboard.html

--- client/js/app.js
+++ client/js/app.js
@@ -4,9 +4,6 @@ var soulFood = angular.module('soulFood', ['ngRoute', 'routeStyles'])
     .when('/', {
       templateUrl: '/partials/main.html', controller: 'mainController', css: 'css/main.css'
     })
-    .when('/main', {
-      templateUrl: '/partials/main.html', controller: 'mainController', css: 'css/main.css'
-    })
     .when('/login', {
       templateUrl: '/partials/login.html', css: 'css/login.css'
     })
@@ -14,10 +11,7 @@ var soulFood = angular.module('soulFood', ['ngRoute', 'routeStyles'])
       templateUrl: '/partials/add.html', css: 'css/add.css'
     })
     .when('/dashboard', {
-      templateUrl: '/partials/TEMPDASHBOARD.html', css: 'css/dash.css'
-    })
-    .when('/tempdashboard', {
-      templateUrl: '/partials/TEMPDASHBOARD.html', css: 'css/dash.css'
+      templateUrl: '/partials/dashboard.html', css: 'css/dash.css'
     })
     .when('/chatRoom', {
       templateUrl: '/partials/chatRoom.html'
